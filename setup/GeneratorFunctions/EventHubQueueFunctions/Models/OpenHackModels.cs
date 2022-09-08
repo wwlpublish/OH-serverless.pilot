a8b3bf3f7aa1ae55b539d2529487beb872c45426
wwlpublish <wwlpub@microsoft.com>
2022-09-08 07:26:41 +0000

    upload html files to github

---
+++ setup/GeneratorFunctions/EventHubQueueFunctions/Models/OpenHackModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ServerlessOpenhack.Models
+{
+    public class OpenHackTeam
+    {
+        public string teamTableNumber { get; set; }
+        public DateTime dateRegistered { get; set; }
+        public string eventHubConnectionString { get; set; }
+        public string eventHubName { get; set; }
+        public string storageAccountConnectionString { get; set; }
+        public string blobContainerName { get; set; }
+        public string ratingEndpoint { get; set; }
+        public bool registeredStorageAccount { get; set; }
+        public bool registeredEventHub { get; set; }
+        public bool registeredRatings { get; set; }
+        public bool eventHubBoost { get; set; }
+        public string contentUrl { get; set; }
+    };
+}
---
+++ setup/ServerlessOpenHackAPI/ServerlessOpenHackAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServerlessOpenHackAPI.Models
+{
+    public class User
+    {
+        public Guid userId;
+        public string userName;
+        public string fullName;
+    }
+}
